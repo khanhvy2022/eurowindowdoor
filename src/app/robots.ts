42e2f0b8d72f3993f3abf8c0d66b70cc2d7e536d
Truong-Luyen <Windowmiennam@gmail.com>
2026-07-29 09:17:27 +0700

    cap nhat seo

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['/api/', '/_next/'],
+        disallow: ['/api/'],
       },
     ],
     sitemap: 'https://eurowindowdoor.com/sitemap.xml',
